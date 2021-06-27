c1bb097a35bf9c11d4311c872f9b3a39511f04ae
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 17:38:14 +0300

    test

---
+++ gittest/target/debug/deps/gittest.d
@@ -0,0 +1,5 @@
+F:\rust\rust_reborn\gittest\target\debug\deps\gittest.exe: src\main.rs
+
+F:\rust\rust_reborn\gittest\target\debug\deps\gittest.d: src\main.rs
+
+src\main.rs:
---
+++ gittest/target/debug/gittest.d
@@ -0,0 +1,1 @@
+F:\rust\rust_reborn\gittest\target\debug\gittest.exe: F:\rust\rust_reborn\gittest\src\main.rs
