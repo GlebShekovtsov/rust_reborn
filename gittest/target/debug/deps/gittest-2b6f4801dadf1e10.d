d07c3c21912f06c3193c037e98fdf789489759c9
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 17:31:51 +0300

    Delete gittest directory

--- gittest/target/debug/deps/gittest-2b6f4801dadf1e10.d
+++
@@ -1,5 +0,0 @@
-f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-2b6f4801dadf1e10.rmeta: src\main.rs
-
-f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-2b6f4801dadf1e10.d: src\main.rs
-
-src\main.rs:
--- gittest/target/debug/deps/gittest-39a99a728a0f2c71.d
+++
@@ -1,5 +0,0 @@
-f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-39a99a728a0f2c71.rmeta: src\main.rs
-
-f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-39a99a728a0f2c71.d: src\main.rs
-
-src\main.rs:
