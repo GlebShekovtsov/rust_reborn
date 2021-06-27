03d4ed2767e9bfdbf68105b823af3022dd2545ff
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 15:49:23 +0300

    initial commit

---
+++ gittest/target/debug/deps/gittest-2b6f4801dadf1e10.d
@@ -0,0 +1,5 @@
+f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-2b6f4801dadf1e10.rmeta: src\main.rs
+
+f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-2b6f4801dadf1e10.d: src\main.rs
+
+src\main.rs:
---
+++ gittest/target/debug/deps/gittest-39a99a728a0f2c71.d
@@ -0,0 +1,5 @@
+f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-39a99a728a0f2c71.rmeta: src\main.rs
+
+f:\rust\github_test\rust_reborn\gittest\target\debug\deps\gittest-39a99a728a0f2c71.d: src\main.rs
+
+src\main.rs:
