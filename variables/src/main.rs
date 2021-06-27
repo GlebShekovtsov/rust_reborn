83128ff01f7952a5e5f7e3ddb91310aca4901bf7
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 20:21:46 +0300

    initial commit

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,7 @@
+//const MAX_POINTS: u32 = 100_000; - константа
 fn main() {
-    let mut x = 5;
-    println!("Значение x равно {}", x);
-    x = 6;
-    println!("Значение x равно {}", x);
-   }
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("Значение х равно {}", x);
+}
