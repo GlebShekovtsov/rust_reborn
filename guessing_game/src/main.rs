975d60e6ba07c79b3a5782248020f06f99043ab5
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 19:14:57 +0300

    initial commit

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,23 +4,28 @@ use rand::Rng;
 fn main() {
  println!("Угадайте число!");
  let secret_number = rand::thread_rng().gen_range(1..101);
+
  loop {
- println!("Пожалуйста, введите свою догадку.");
- let mut guess = String::new();
- io::stdin().read_line(&mut guess)
- .expect("Не получилось прочитать строку");
- let guess: u32 = match guess.trim().parse() {
- Ok(num) => num,
- Err(_) => continue,
-};
-println!("Вы загадали: {}", guess);
-match guess.cmp(&secret_number) {
-Ordering::Less => println!("Слишком малое число!"),
-Ordering::Greater => println!("Слишком большое число!"),
-Ordering::Equal => {
-println!("Вы выиграли!");
-break;
-}
-}
-}
+    println!("Пожалуйста, введите свою догадку.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+    .expect("Не получилось прочитать строку");
+
+        let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+        };
+
+    println!("Вы загадали: {}", guess);
+
+    match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Слишком малое число!"),
+    Ordering::Greater => println!("Слишком большое число!"),
+    
+    Ordering::Equal => {
+    println!("Вы выиграли!");
+    break;
+            }
+        }
+    }
 }
