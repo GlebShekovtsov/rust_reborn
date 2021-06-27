f6e96c4a5c351f06c841c71015ee120abed91de0
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-06-27 22:44:39 +0300

    initial commit

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,5 +3,8 @@ fn main() {
     let x = 5;
     let x = x + 1;
     let x = x * 2;
+    let spaces = " ";
+    let spaces = spaces.len();
     println!("Значение х равно {}", x);
+    let guess: u32 = "42".parse().expect("Не является числом!");
 }
